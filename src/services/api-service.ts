57c67b37b3cf3dfde4b81365321693262f0f2c89
Jekman <jeka_zzz@mail.ru>
2020-09-11 21:06:10 +0300

    api-service/feat: create all methods for api

--- src/services/api-service.ts
+++ src/services/api-service.ts
@@ -2,130 +2,112 @@ export default class ApiService {
 
   _apiBase = 'https://rs-react-schedule.firebaseapp.com/api/team/team10';
 
-  /*
-  /team/{teamId}/events Get events from the store
-  /team/{teamId}/event Add a new event to the store
-  /team/{teamId}/event/{eventId} Find event by ID
-  /team​/{teamId}​/event​/{eventId} Update an existing event
-  /team/{teamId}/event/{eventId} Delete event by ID
-  /team/{teamId}/organizers Get organizers from the store
-  /team/{teamId}/organizer Add a new organizer
-  /team/{teamId}/organizer/{organizerId} Find organizer by ID
-  /team/{teamId}/organizer/{organizerId} Update an existing organizer
-  /team/{teamId}/organizer/{organizerId} Delete organizer by ID
-  */
-
-  getAllEvents = async () => {
-    const res = await fetch(`${this._apiBase}/events `);
+  getAllData = async (url: string) => {
+    const res = await fetch(`${this._apiBase}${url}`);
 
     if (!res.ok) {
-      throw new Error(`Could not fetch events` +
-        `, received ${res.status}`)
+      throw new Error(`Server error, received ${res.status}`)
     }
 
     return await res.json();
   };
 
+  createData = async (url: string, data: object) => {
+    const res = await fetch(`${this._apiBase}${url}`, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    });
 
+    if (!res.ok) {
+      throw new Error(`Server error, received ${res.status}`)
+    }
 
-  // getResource = async (url: any) => {
-  //   const res = await fetch(`${this._apiBase}${url}`);
+    return await res.json();
+  };
 
-  //   if (!res.ok) {
-  //     throw new Error(`Could not fetch ${url}` +
-  //       `, received ${res.status}`)
-  //   }
-  //   return await res.json();
-  // };
+  getDataById = async (url: string, id: string) => {
+    const res = await fetch(`${this._apiBase}${url}${id}`);
 
-  // getAllPeople = async () => {
-  //   const res = await this.getResource(`/people/`);
-  //   return res.results
-  //     .map(this._transformPerson)
-  //     .slice(0, 5);
-  // };
+    if (!res.ok) {
+      throw new Error(`Server error, received ${res.status}`)
+    }
 
-  // getPerson = async (id: any) => {
-  //   const person = await this.getResource(`/people/${id}/`);
-  //   return this._transformPerson(person);
-  // };
+    return await res.json();
+  };
 
-  // getAllPlanets = async () => {
-  //   const res = await this.getResource(`/planets/`);
-  //   return res.results
-  //     .map(this._transformPlanet)
-  //     .slice(0, 5);
-  // };
+  updateData = async (url: string, id: string, data: object) => {
+    const res = await fetch(`${this._apiBase}${url}${id}`, {
+      method: 'PUT',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    });
 
-  // getPlanet = async (id: any) => {
-  //   const planet = await this.getResource(`/planets/${id}/`);
-  //   return this._transformPlanet(planet);
-  // };
+    if (!res.ok) {
+      throw new Error(`Server error, received ${res.status}`)
+    }
 
-  // getAllStarships = async () => {
-  //   const res = await this.getResource(`/starships/`);
-  //   return res.results
-  //     .map(this._transformStarship)
-  //     .slice(0, 5);
-  // };
+    return await res.json();
+  };
 
-  // getStarship = async (id: any) => {
-  //   const starship = await this.getResource(`/starships/${id}/`);
-  //   return this._transformStarship(starship);
-  // };
+  deleteData = async (url: string, id: string) => {
+    const res = await fetch(`${this._apiBase}${url}${id}`, {
+      method: 'DELETE',
+      headers: {
+        Accept: 'application/json',
+      },
+    });
 
-  // getPersonImage = ({ id }: any) => {
-  //   return `${this._imageBase}/characters/${id}.jpg`
-  // }
+    if (!res.ok) {
+      throw new Error(`Server error, received ${res.status}`)
+    }
 
-  // getStarshipImage = ({ id }: any) => {
-  //   return `${this._imageBase}/starships/${id}.jpg`
-  // }
+    return await res.json();
+  };
 
-  // getPlanetImage = ({ id }: any) => {
-  //   return `${this._imageBase}/planets/${id}.jpg`
-  // }
+  getAllEvents = async () => {
+    return await this.getAllData(`/events`);
+  };
 
-  // _extractId = (item: { url: { match: (arg0: RegExp) => any[]; }; }) => {
-  //   const idRegExp = /\/([0-9]*)\/$/;
-  //   return item.url.match(idRegExp)[1];
-  // };
+  createEvent = async (newEvent: object) => {
+    return await this.createData(`/event/`, newEvent);
+  };
 
-  // _transformPlanet = (planet: { name: any; diameter: any; gravity: any; orbital_period: any; population: any; terrain: any; climate: any; rotation_period: any; }) => {
-  //   return {
-  //     id: this._extractId(planet),
-  //     name: planet.name,
-  //     diameter: planet.diameter,
-  //     gravity: planet.gravity,
-  //     orbitalPeriod: planet.orbital_period,
-  //     population: planet.population,
-  //     terrain: planet.terrain,
-  //     climate: planet.climate,
-  //     rotationPeriod: planet.rotation_period,
-  //   };
-  // };
+  getEventById = async (id: string) => {
+    return await this.getDataById(`/event/`, id);
+  };
 
-  // _transformStarship = (starship: { name: any; model: any; manufacturer: any; cost_in_credits: any; length: any; crew: any; passengers: any; cargo_capacity: any; }) => {
-  //   return {
-  //     id: this._extractId(starship),
-  //     name: starship.name,
-  //     model: starship.model,
-  //     manufacturer: starship.manufacturer,
-  //     costInCredits: starship.cost_in_credits,
-  //     length: starship.length,
-  //     crew: starship.crew,
-  //     passengers: starship.passengers,
-  //     cargoCapacity: starship.cargo_capacity
-  //   }
-  // };
+  updateEvent = async (id: string, newEvent: object) => {
+    return await this.updateData(`/event/`, id, newEvent);
+  };
 
-  // _transformPerson = (person: { name: any; gender: any; birth_year: any; eye_color: any; }) => {
-  //   return {
-  //     id: this._extractId(person),
-  //     name: person.name,
-  //     gender: person.gender,
-  //     birthYear: person.birth_year,
-  //     eyeColor: person.eye_color
-  //   }
-  // }
+  deleteEvent = async (id: string) => {
+    return await this.deleteData(`/event/`, id);
+  };
+
+  getAllOrganizers = async () => {
+    return await this.getAllData(`/organizers`);
+  };
+
+  createOrganizer = async (newOrganizer: object) => {
+    return await this.createData(`/organizer/`, newOrganizer);
+  };
+
+  getOrganizerById = async (id: string) => {
+    return await this.getDataById(`/organizer/`, id);
+  };
+
+  updateOrganizer = async (id: string, newOrganizer: object) => {
+    return await this.updateData(`/organizer/`, id, newOrganizer);
+  };
+
+  deleteOrganizer = async (id: string) => {
+    return await this.deleteData(`/organizer/`, id);
+  };
 }

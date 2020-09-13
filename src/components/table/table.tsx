3bbca62ccc90fa1f4336afb363abaff2d9ea63d2
Alexandr-Voytekhovich <avth.tec@gmail.com>
2020-09-14 01:19:35 +0300

    Table-component/feat: add new properties

--- src/components/table/table.tsx
+++ src/components/table/table.tsx
@@ -1,27 +1,63 @@
 import React from 'react';
-
 import { Table } from 'antd';
+import { Breakpoint } from 'antd/lib/_util/responsiveObserve';
 
 import './table.scss';
 
+
+
 const columns = [
-  { title: 'Name', dataIndex: 'name', key: 'name' },
+  { 
+    title: 'Name', 
+    dataIndex: 'name', 
+    key: 'name',
+    align: 'center' as 'right'
+  },
   {
     title: 'DescriptionUrl',
     dataIndex: 'descriptionUrl',
     key: 'descriptionUrl',
-    render: (text:any) => <a href={text} rel="noopener noreferrer" target="_blank">GitHub</a>
+    align: 'center' as 'right',
+    render: (text:any) => <a href={text} rel="noopener noreferrer" target="_blank">GitHub</a>,
   },
-  { title: 'Type', dataIndex: 'type', key: 'type' },
-  { title: 'TimeZone', dataIndex: 'timeZone', key: 'timeZone' },
-  { title: 'Place', dataIndex: 'place', key: 'place' },
-  { title: 'Comment', dataIndex: 'comment', key: 'comment' },
+  { 
+    itle: 'Type', 
+    dataIndex: 'type', 
+    key: 'type',
+    align: 'center' as 'right',
+    responsive: ['lg'] as Breakpoint[]
+  },
+  { 
+    title: 'TimeZone', 
+    dataIndex: 'timeZone', 
+    key: 'timeZone',
+    align: 'center' as 'right',
+    responsive: ['lg'] as Breakpoint[]
+  },
+  { 
+    title: 'Place', 
+    dataIndex: 'place', 
+    key: 'place',
+    align: 'center' as 'right',
+    responsive: ['lg'] as Breakpoint[]
+  },
+  { 
+    title: 'Description', 
+    dataIndex: 'description', 
+    key: 'description',
+  },
+  { 
+    title: 'Comment', 
+    dataIndex: 'comment', 
+    key: 'comment',
+    responsive: ['lg'] as Breakpoint[]
+  }
 ];
 
 const data = [
   {
-    key: 1,
-    name: 'SongBird',
+    key: '1',
+    name: 'SongBird1',
     description: 'Songbird - одностраничное приложение, викторина для распознавания птиц по их голосам',
     descriptionUrl: 'https://github.com/rolling-scopes-school/tasks/blob/master/tasks/songbird.md',
     type: 'Task',
@@ -30,8 +66,8 @@ const data = [
     comment: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere est esse fugit mollitia aut.',
   },
   {
-    key: 2,
-    name: 'SongBird',
+    key: '2',
+    name: 'SongBird2',
     description: 'Songbird - одностраничное приложение, викторина для распознавания птиц по их голосам',
     descriptionUrl: 'https://github.com/rolling-scopes-school/tasks/blob/master/tasks/songbird.md',
     type: 'Task',
@@ -40,8 +76,8 @@ const data = [
     comment: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere est esse fugit mollitia aut.',
   },
   {
-    key: 3,
-    name: 'SongBird',
+    key: '3',
+    name: 'SongBird3',
     description: 'Songbird - одностраничное приложение, викторина для распознавания птиц по их голосам',
     descriptionUrl: 'https://github.com/rolling-scopes-school/tasks/blob/master/tasks/songbird.md',
     type: 'Task',
@@ -50,8 +86,8 @@ const data = [
     comment: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere est esse fugit mollitia aut.',
   },
   {
-    key: 4,
-    name: 'SongBird',
+    key: '4',
+    name: 'SongBird4',
     description: 'Songbird - одностраничное приложение, викторина для распознавания птиц по их голосам',
     descriptionUrl: 'https://github.com/rolling-scopes-school/tasks/blob/master/tasks/songbird.md',
     type: 'Task',
@@ -60,8 +96,8 @@ const data = [
     comment: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere est esse fugit mollitia aut.',
   },
   {
-    key: 5,
-    name: 'SongBird',
+    key: '5',
+    name: 'SongBird5',
     description: 'Songbird - одностраничное приложение, викторина для распознавания птиц по их голосам',
     descriptionUrl: 'https://github.com/rolling-scopes-school/tasks/blob/master/tasks/songbird.md',
     type: 'Task',
@@ -75,27 +111,38 @@ interface Props {
   appData: any[]
 }
 
+const rowSelection = {
+  onChange: (selectedRowKeys:any, selectedRows:any) => {
+    console.log(`selectedRowKeys: ${selectedRowKeys}`, 'selectedRows: ', selectedRows);
+  },
+  getCheckboxProps: (record:any) => ({
+    disabled: record.name === 'Disabled User', // Column configuration not to be checked
+    name: record.name,
+  }),
+};
+  
 const TableComponent: React.FunctionComponent<Props> = ({ appData }) => {
+  const selectionType = 'checkbox';
+
   return (
-    <Table
+    <div>
+      <Table
+        pagination={{ 
+          pageSize: 3,
+          position: ['bottomCenter']
+         }}
+        size="middle"
+        rowSelection={{
+          type: selectionType,
+          ...rowSelection,
+        }}
 
-    columns={columns}
+        columns={columns}
 
-    expandable={{
-      expandedRowRender: record => {
-        return (
-          <>
-            <h1 style={{ margin: 0 }}>{record.description}</h1>
-            <img src="https://raw.githubusercontent.com/rolling-scopes-school/tasks/master/tasks/songbird/screenshot_birds-quiz.png" alt="13"></img>
-          </>
-        )
-      } ,
-      rowExpandable: record => record.name !== 'Not Expandable',
-    }}
-
-    dataSource={data}
-  />
+        dataSource={data}
+      />
+    </div>
   );
-}
+};
 
 export default TableComponent;

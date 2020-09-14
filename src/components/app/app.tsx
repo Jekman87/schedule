99a874587c04c10eaa94096256f127198daa8e5c
Jekman <jeka_zzz@mail.ru>
2020-09-14 05:00:27 +0300

    App/feat: add settings methods

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -17,17 +17,31 @@ import 'antd/dist/antd.css'
 
 const api = new ApiService();
 
+//создать константы для настроек. Используем здесь, в хедере и возможно в др местах
+
+const initialSettings = {};
+const savedSettings = {} || null; // забираем из LocalStorage
+const currentSettings = savedSettings ? savedSettings : initialSettings;
+
 // const createEvent = (newEvent: object) => api.createEvent(newEvent).then((data) => console.log(data)); // pass to hedear + call modal window + time-zone + eye +
 // const getEventById = (id: string) => api.getEventById(id).then((data) => console.log(data));
 // const updateEvent = (id: string, newEvent: object) => api.updateEvent(id, newEvent).then((data) => console.log(data)); // pass to all (without header)
 // const deleteEvent = (id: string) => api.deleteEvent(id).then((data) => console.log(data)); // pass to all (without header)
 
 const App: React.FC = () => {
+  // при обновлении страницы настройки берутся из LocalStorage - currentSettings
 
-  const [workSpace, setWorkSpace] = useState<string>('table');
   const [appData, setAppData] = useState<string[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<boolean>(false);
+
+  // объединить настройки в 1 объект стейт?
+  //const [settings, setSettings] = useState<{}>(currentSettings);
+
+  const [workSpace, setWorkSpace] = useState<string>('table');
+  const [role, setRole] = useState<string>('student');
+  const [accessibility, setAccessibility] = useState<boolean>(false);
+  const [timeZone, setTimeZone] = useState<string>('ru-Ru');
 
   useEffect(() => {
     api.getAllEvents()
@@ -42,9 +56,52 @@ const App: React.FC = () => {
       });
   }, []);
 
-  const changeWorkSpace = (clickedWorkSpace:string):void => {
+  const changeWorkSpace = (clickedWorkSpace: string):void => {
     setWorkSpace(clickedWorkSpace);
   }
+
+  const changeRole = (role: string):void => {
+    setRole(role);
+    console.log('Setting changeRole: ', role);
+  }
+
+  const changeAccessibility = (isOn: boolean):void => {
+    setAccessibility(isOn);
+    console.log('Setting changeAccessibility: ', isOn);
+  }
+
+  const changeTimeZone= (timeZone: string):void => {
+    setTimeZone(timeZone);
+    console.log('Setting changeTimeZone: ', timeZone);
+  }
+
+  const saveSchedule = (format: string):void => {
+    // созранение расписание в зависимости от формата
+    console.log('Setting saveSchedule: ', format);
+  }
+
+  const changeStyles= (style: string):void => {
+    // setStyles(style);
+    console.log('Setting changeStyles: ', style);
+  }
+
+  const changeVisibility= (visibility: string):void => {
+    // setVisibility(visibility);
+    console.log('Setting changchangeVisibilityeTimeZone: ', visibility);
+  }
+
+  // метод вызывается из модалки при создании нового события
+  const createEvent = (newEvent: object):void => {
+    // api.createEvent(newEvent)
+    console.log('Setting addEvent');
+  }
+
+  // метод вызывается из модалки при редактировании события. Или из таблицы.
+  const updateEvent = (id: string, newEvent: object):void => {
+    // api.updateEvent(id, newEvent)
+    console.log('Setting addEvent');
+  }
+
 
   const addWorkSpace = (currentWorkSpace:string) => {
     console.log(appData);
@@ -71,8 +128,25 @@ const App: React.FC = () => {
   return (
     <>
       <Header
-        onChangeWorkSpace={changeWorkSpace}
+        /*
+        settings={settings}
 
+        workSpace={workSpace}
+        role={role}
+        accessibility={accessibility}
+        timeZone={timeZone}
+        */
+
+        onChangeWorkSpace={changeWorkSpace}
+        /*
+        changeWorkSpace={changeWorkSpace}
+        changeRole={changeRole}
+        changeAccessibility={changeAccessibility}
+        changeTimeZone={changeTimeZone}
+        saveSchedule={saveSchedule}
+        changeStyles={changeStyles}
+        changeVisibility={changeVisibility}
+        */
       />
       {errorMessage}
       {spinner}

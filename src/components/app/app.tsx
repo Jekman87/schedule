90bf7deddfee719dcdede0afec54d7d259125fae
Jekman <jeka_zzz@mail.ru>
2020-09-14 03:51:58 +0300

    App/feat: add Spinner

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -7,42 +7,48 @@ import TableComponent from '../table';
 import SList from '../list';
 import Calendar from '../calendar';
 import Footer from '../footer'
+import Spinner from '../spinner';
 // import InfoWindow from '../info-window'
 // import EditWindow from '../edit-window'
 
 import './app.scss';
 import 'antd/dist/antd.css'
 
-import 'antd/dist/antd.css'
-
 
 const api = new ApiService();
 
-const createEvent = (newEvent: object) => api.createEvent(newEvent).then((data) => console.log(data)); // pass to hedear + call modal window + time-zone + eye +
+// const createEvent = (newEvent: object) => api.createEvent(newEvent).then((data) => console.log(data)); // pass to hedear + call modal window + time-zone + eye +
 // const getEventById = (id: string) => api.getEventById(id).then((data) => console.log(data));
-const updateEvent = (id: string, newEvent: object) => api.updateEvent(id, newEvent).then((data) => console.log(data)); // pass to all (without header)
-const deleteEvent = (id: string) => api.deleteEvent(id).then((data) => console.log(data)); // pass to all (without header)
+// const updateEvent = (id: string, newEvent: object) => api.updateEvent(id, newEvent).then((data) => console.log(data)); // pass to all (without header)
+// const deleteEvent = (id: string) => api.deleteEvent(id).then((data) => console.log(data)); // pass to all (without header)
 
+const App: React.FC = () => {
 
-const App: React.FunctionComponent = () => {
-
-  const [workSpace, setWorkSpace] = useState<string>('table')
-  const [appData, setAppData] = useState<any[]>([])
+  const [workSpace, setWorkSpace] = useState<string>('table');
+  const [appData, setAppData] = useState<string[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<boolean>(false);
 
   useEffect(() => {
-    api.getAllEvents().then((data) => setAppData(data.data)); // to state
-    api.getEventById("CendyYTicEvwd0rofuDD")
+    api.getAllEvents()
+      .then((data) => {
+        setLoading(false);
+        setAppData(data.data);
+      })
+      .catch((err) => {
+        console.log(err);
+        setLoading(false);
+        setError(true);
+      });
   }, []);
 
-  useEffect(() => {
-    console.log(appData)
-  });
-
-  const changeWorkSpace = (clickedWorkSpace:any):void => {
-    setWorkSpace(clickedWorkSpace)
+  const changeWorkSpace = (clickedWorkSpace:string):void => {
+    setWorkSpace(clickedWorkSpace);
   }
 
   const addWorkSpace = (currentWorkSpace:string) => {
+    console.log(appData);
+
     switch(currentWorkSpace) {
       case 'table':
         return <TableComponent
@@ -54,19 +60,27 @@ const App: React.FunctionComponent = () => {
         return <Calendar
                 appData={appData} />
       default:
-        console.log(currentWorkSpace)
+        return null;
     }
   }
 
+  const errorMessage = error ? <p>Server error!</p> : null;
+  const spinner = loading ? <Spinner /> : null;
+  const content = appData.length ? addWorkSpace(workSpace) : null;
+
   return (
-    <div className="app">
+    <>
       <Header
-      onChangeWorkSpace={changeWorkSpace} />
-      { addWorkSpace(workSpace) }
+        onChangeWorkSpace={changeWorkSpace}
+
+      />
+      {errorMessage}
+      {spinner}
+      {content}
       <Footer />
       {/* <InfoWindow />
       <EditWindow /> */}
-    </div>
+    </>
   );
 }
 

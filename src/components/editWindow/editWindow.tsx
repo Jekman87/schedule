67985abea8c07a17cd2bfc56ab29c8269e8a02f3
NatalliaShulgach <nataredred@gmail.com>
2020-09-16 12:52:43 +0300

    EditWindow-component/feat: create neew object

--- src/components/editWindow/editWindow.tsx
+++ src/components/editWindow/editWindow.tsx
@@ -1,15 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Form,
   Input,
   Button,
   Select,
-  Cascader,
+  // Cascader,
   DatePicker,
-  InputNumber,
-  Switch,
+  // InputNumber,
+  // Switch,
   Typography,
 } from 'antd';
+import ApiService from '../../services/api-service';
 
 import './editWindow.scss';
 
@@ -18,89 +19,105 @@ const { RangePicker } = DatePicker;
 const { TextArea } = Input;
 
 
-function EditWindow() {
-    return (
-        <div className='editWindow'>
-            <Title level={3}>Добавить задание</Title>
-          <Form
-            labelCol={{ span: 4 }}
-            wrapperCol={{ span: 14 }}
-            layout="horizontal"
-            className='editWindow-form'
-          >
-            <Form.Item label="Название">
-              <Input />
-            </Form.Item>
-            <Form.Item label="Тип">
-              <Cascader
-              
-                options={[
-                  {
-                    value: 'task',
-                    label: 'task',
-                    children: [
-                      {
-                        value: 'markup-task',
-                        label: 'markup-task',
-                      },
-                      {
-                        value: 'js-task',
-                        label: 'js-task',
-                      },
-                      {
-                        value: 'git-task',
-                        label: 'git-task',
-                      },
-                      {
-                        value: 'codewars-task',
-                        label: 'codewars-task',
-                      },
-                    ],
-                  },
-                  {
-                      value: 'event',
-                      label: 'event',
-                        children: [
-                        {
-                            value: 'online',
-                            label: 'online',
-                        },
-                        {
-                            value: 'offline',
-                            label: 'offline',
-                        },
-                    ],
-                  },
-                  {
-                    value: 'lecture',
-                    label: 'lecture',
-                  },
-                  {
-                    value: 'test',
-                    label: 'test',
-                  }
-                ]}
-              />
-            </Form.Item>
-            <Form.Item label="Дата">
-                <RangePicker showTime format="YYYY-MM-DD HH:mm:ss" />
-            </Form.Item>
-            <Form.Item label="Ссылка">
-              <Input />
-            </Form.Item>
-            <Form.Item label="Комментарий">
-                <TextArea
-                    placeholder="Autosize height with minimum and maximum number of lines"
-                    autoSize
-                />
-            </Form.Item>
-            <Form.Item>
-                <Button type="primary" block>Submit</Button>
-            </Form.Item>
-          </Form>
-        </div>
-      );
-    
-}
+const EditWindow: React.FunctionComponent = () => {
+  const api = new ApiService();
+  const getAllEvents = () => api.getAllEvents().then((data) => console.log(data));
+  const createEvent = (newEvent: object) => api.createEvent(newEvent).then((data) => console.log(data));
 
+  function dataUpdate() {
+    let arrayEvents = getAllEvents();
+    arrayEvents += objEvent;
+    return arrayEvents;
+  }
+
+  const [newName, setNewName] = useState('');
+
+  const [newDescription, setNewDescription] = useState('');
+  const [newType, setNewType] = useState('');
+  const [newPlace, setNewPlace] = useState('');
+  const [newComment, setNewComment] = useState('');
+  // const [newStartTask, setNewStartTask] = useState('');
+  // const [newDedlineTask, setNewDedlineTask] = useState('');
+
+  const objEvent: any = {
+    name: newName,
+    key: newName + new Date(),
+    description: newDescription,
+    type: newType,
+    place: newPlace,
+    comment: newComment,
+    startTask: "start date",
+    dedlineTask: "dedline",
+  }
+
+  const FORM: any = (
+    <div>
+      <Title level={3}>Add new task</Title>
+      <Form
+        labelCol={{ span: 4 }}
+        wrapperCol={{ span: 14 }}
+        layout="horizontal"
+        className='editWindow-form'
+      >
+        <Form.Item label="Title new task">
+          <Input
+            value={newName}
+            onChange={(el) => setNewName((el.target as HTMLInputElement).value)}
+          />
+        </Form.Item>
+        <Form.Item label="Type task">
+          <Select onChange={(e) => setNewType(e as HTMLSelectElement["value"])}>
+            <Select.Option key={"Markup-task"} value={"Markup-task"}>Markup-task</Select.Option>
+            <Select.Option key={"Js-task"} value={"Js-task"}>Js-task</Select.Option>
+            <Select.Option key={"Git-task"} value={"Git-task"}>Git-task</Select.Option>
+            <Select.Option key={"Codewars-task"} value={"Codewars-task"}>Codewars-task</Select.Option>
+            <Select.Option key={"Other"} value={"Other"}>Other</Select.Option>
+          </Select>
+        </Form.Item>
+
+
+        <Form.Item label="Task completion period">
+          <RangePicker showTime format="YYYY-MM-DD HH:mm:ss" />
+        </Form.Item>
+        <Form.Item label="Link to GitHub">
+          <Input
+            value={newPlace}
+            onChange={(el) => setNewPlace((el.target as HTMLInputElement).value)} />
+        </Form.Item>
+        <Form.Item label="Description">
+          <TextArea
+            placeholder="Autosize height with minimum and maximum number of lines"
+            autoSize
+            value={newDescription}
+            onChange={(el) => setNewDescription((el.target as HTMLTextAreaElement).value)}
+          />
+        </Form.Item>
+        <Form.Item label="Сomment">
+          <TextArea
+            placeholder="Autosize height with minimum and maximum number of lines"
+            autoSize
+            value={newComment}
+            onChange={(el) => setNewComment((el.target as HTMLTextAreaElement).value)}
+          />
+        </Form.Item>
+
+        <Form.Item>
+          <Button type="primary" onClick={() => {
+            dataUpdate();
+            createEvent(objEvent)
+          }}>Save data</Button>
+        </Form.Item>
+        <Form.Item>
+          <Button type="primary">Preview</Button>
+        </Form.Item>
+      </Form>
+    </div>
+  )
+
+  return (
+    <div className='editWindow'>
+      {FORM}
+    </div>
+  );
+}
 export default EditWindow;

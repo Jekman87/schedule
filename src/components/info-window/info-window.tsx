bd14fa3dada4225c136f5ef0536071ed84bcb849
ayaki-coder <sj.a.yakimovich@gmail.com>
2020-09-27 07:22:21 +0300

    refactor: delete unused vars and comments

--- src/components/info-window/info-window.tsx
+++ src/components/info-window/info-window.tsx
@@ -1,6 +1,6 @@
 import { Button, Badge, Card, Popover, Timeline, Row, Col, Tag, Divider } from 'antd';
 import Modal from 'antd/lib/modal/Modal';
-import React, { useState } from 'react';
+import React from 'react';
 import { Typography } from 'antd';
 import './info-window.scss';
 import {
@@ -20,9 +20,8 @@ import {
   HistoryOutlined
 } from '@ant-design/icons';
 import { convertDateTime } from '../../helpers/utils';
-import { red } from '@ant-design/colors';
 
-const { Title, Text, Link, Paragraph } = Typography;
+const { Title, Link, Paragraph } = Typography;
 
 interface Props {
   event: any;
@@ -32,11 +31,8 @@ interface Props {
   closeModal: any;
 }
 
-// ДОЛЖЕН ПРИХОДИТЬ ИВЕНТ В ПРОПСАХ
-
 const InfoWindow: React.FunctionComponent<Props> = ({ settings, event, closeModal, ...props }: Props) => {
-  // const { event } = props;
-  // const [visible, setVisible] = useState(true);
+  
   const stage = event.stage === '' ? '' : ` Stage#${event.stage}`;
 
   const CreateLink = (link: any, text: any) => {
@@ -54,48 +50,9 @@ const InfoWindow: React.FunctionComponent<Props> = ({ settings, event, closeModa
     );
   }
 
-
-
-
   console.log('info-window props', event, props);
 
-  //  return <div></div>;
-
   return (
-    // <div className="info-window">
-    //    <div className="modal-window">
-    //      <div className="ant-modal-content">
-    //        <button onClick={() => closeModal()} type="button" aria-label="Close" className="ant-modal-close">
-    //          <span className="ant-modal-close-x">
-    //            <span role="img" aria-label="close" className="anticon anticon-close ant-modal-close-icon">
-    //              <svg viewBox="64 64 896 896" focusable="false" className="" data-icon="close" width="1em" height="1em" fill="currentColor" aria-hidden="true">
-    //                <path d="M563.8 512l262.5-312.9c4.4-5.2.7-13.1-6.1-13.1h-79.8c-4.7 0-9.2 2.1-12.3 5.7L511.6 449.8 295.1 191.7c-3-3.6-7.5-5.7-12.3-5.7H203c-6.8 0-10.5 7.9-6.1 13.1L459.4 512 196.9 824.9A7.95 7.95 0 00203 838h79.8c4.7 0 9.2-2.1 12.3-5.7l216.5-258.1 216.5 258.1c3 3.6 7.5 5.7 12.3 5.7h79.8c6.8 0 10.5-7.9 6.1-13.1L563.8 512z">
-    //                </path>
-    //              </svg>
-    //            </span>
-    //          </span>
-    //        </button>
-    //        <div className="ant-modal-header">
-    //          <div className="ant-modal-title">
-    //           RSS {event.course}{stage}
-    //          </div>
-    //        </div>
-    //        <div className="ant-modal-body">
-    //          <p className="ant-modal-title">TASKS NOT COMPLETED</p>
-    //          <div className="ant-table-wrapper">
-    //            <div className="ant-spin-nested-loading">
-    //              <div className="ant-spin-container">
-    //                {/* {event} */}
-    //              </div>
-    //            </div>
-    //          </div>
-    //        </div>
-    //      </div>
-    //   </div>
-    // </div>
-
-
-
     <Modal
       title={`RSS ${event.course}${stage}`}
       centered
@@ -104,7 +61,7 @@ const InfoWindow: React.FunctionComponent<Props> = ({ settings, event, closeModa
       footer={null}
       keyboard={true}
       bodyStyle={{ backgroundColor: '#ffe7e3', }}
-      //bodyStyle= здесь функция которая навешивает цсс свойства в зависимости от типа ивента
+      //bodyStyle= здесь функция которая навешивает цсс свойства в зависимости от типа ивента или попробовать навесить классы готовой функцией из апп
       width={"80%"}
     >
       <Badge.Ribbon text={event.form} color={'#45e21e'}>
@@ -136,31 +93,33 @@ const InfoWindow: React.FunctionComponent<Props> = ({ settings, event, closeModa
 
       <Row justify="space-between">
         <Col span={12}>
+          {/* не рендерить этот параграф если нет даты старта */}
           <Paragraph className="modal-font">
             <span className="icon start-icon"><CalendarOutlined /></span>
             <i>start: </i><strong> {convertDateTime(event.dateTime, false, settings.timeZone)}   <span className="start-icon"><ClockCircleOutlined /></span> {convertDateTime(event.dateTime, true, settings.timeZone)}</strong>
           </Paragraph>
+          {/* не рендерить этот параграф если нет даты дедлайна */}
           <Paragraph type="danger" className="modal-font">
             <span className="icon deadline-icon"><CalendarOutlined /></span>
             <i>deadline: </i><strong>{convertDateTime(event.deadlinedateTime, false, settings.timeZone)}   <span><ClockCircleOutlined /> </span>{convertDateTime(event.deadlinedateTime, true, settings.timeZone)}</strong>
           </Paragraph>
+          {/* не рендерить этот параграф если нет дюрэйшн */}
           <Paragraph className="modal-font">
             <span className="icon usual-icon"><HistoryOutlined /></span>
             <i>it can take you about </i><strong>{event.duration}</strong>
           </Paragraph>
+          {/* не рендерить этот параграф если нет орга, разсплитить и сджойнить имена  */}
           <Paragraph className="modal-font">
             <span className="icon usual-icon"><UserOutlined /></span>
             <i>organizer: </i><span>{event.organizer}</span>
           </Paragraph>
-          {/* <Timeline mode={"left"}>
-            <Timeline.Item color='green' label={event.dateTime}>Start</Timeline.Item>
-            <Timeline.Item color='red' dot={<ClockCircleOutlined style={{ fontSize: '16px' }} />} label={event.deadlinedateTime}>Deadline</Timeline.Item>
-          </Timeline> */}
         </Col>
         <Col span={8}>
+        {/* не рендерить этот параграф если нет, сделать 1 функцию на прием текста и параметра на эти 3 параграфа */}
           <Paragraph className="modal-font"><i>kind:</i> <span>{event.kind}</span></Paragraph>
           <Paragraph className="modal-font"><i>type:</i> <span>{event.type}</span></Paragraph>
           <Paragraph className="modal-font"><i>form:</i> <span>{event.form}</span></Paragraph>
+          {/* не рендерить если нет, переделать на CreateLink  я ее уже написала */}
           <Popover placement="bottomLeft" content={<Link href={event.eventURL} target="_blank">{event.eventURL}</Link>} trigger="hover">
             <Paragraph className="modal-font">
               <Link className="modal-font" href={event.eventURL} target="_blank">
@@ -170,39 +129,30 @@ const InfoWindow: React.FunctionComponent<Props> = ({ settings, event, closeModa
                 </Link>
             </Paragraph>
           </Popover>
-
-          {/* <Link href={event.eventURL} target="_blank">
-            <span className="icon duration-icon"><LinkOutlined /></span>link to event
-          </Link> */}
         </Col>
-
-
-        {/* <Col span={10}>
-          <a href={event.eventURL}> link to event</a>
-          <p>{event.description}</p>
-          здесь нужно размапить массив тегов выбрать им иконку и цвет
-          <Tag icon={< ClockCircleOutlined />} color="success">
-            {event.tags}
-          </Tag>
-        </Col> */}
       </Row>
+
       <Row>
         <Typography>
           <Title level={3}>Description</Title>
           <Paragraph>
-            <Tag icon={< ClockCircleOutlined />} color="success">
+          {/* здесь нужно размапить массив тегов выбрать на каждый вид цвет */}
+            <Tag color="success">
               {event.tags}
             </Tag>
           </Paragraph>
+          {/* не рендерить если нет  */}
           <Paragraph className="modal-font">
             {event.description}
 
           </Paragraph>
+           {/* не рендерить если нет*/}
           {CreateLink(event.descriptionUrl, 'link to description')}
           {/* <Paragraph className="modal-font"><Link className="modal-font" href={event.descriptionUrl} target="_blank">
             <span className="icon usual-icon"><LinkOutlined /></span>link to description
           </Link>
           </Paragraph> */}
+          {/* не рендерить если нет картинок ссылок или видео размапить массив */}
           <Title level={4}>Materials:</Title>
           {/* <Paragraph className="modal-font"><Link className="modal-font" href={event.materials.links[0].link} target="_blank">
             <span className="icon duration-icon"><LinkOutlined /></span>{event.materials.links[0].discription}
@@ -210,7 +160,7 @@ const InfoWindow: React.FunctionComponent<Props> = ({ settings, event, closeModa
           </Paragraph> */}
         </Typography>
       </Row>
-
+          {/* не рендерить если нет*/}
       <Row>
         <Typography>
           <Title level={3}>Description of Deadline</Title>
@@ -219,6 +169,7 @@ const InfoWindow: React.FunctionComponent<Props> = ({ settings, event, closeModa
           </Paragraph>
         </Typography>
       </Row>
+      {/* доделать фидбэк*/}
       <Row>
         <Typography>
           <div className="feedback-title-wrapper">
@@ -230,28 +181,6 @@ const InfoWindow: React.FunctionComponent<Props> = ({ settings, event, closeModa
           </Paragraph>
         </Typography>
       </Row>
-
-
-      {/* <div>dateTime {event.dateTime}</div>
-      <div>deadlinedateTime {event.deadlinedateTime}</div>
-      <div>eventURL {event.eventURL}</div>
-      <div>description{event.description}</div>
-      <div>descriptionUrl{event.descriptionUrl}</div>
-      <div> duration {event.duration}</div>
-      <div>form {event.form}</div>
-      <div>kind {event.kind}</div>
-      {/* <div>{event.material.links[0].link}{event.material.links[0].description}</div>
-        <div>{event.material.video[0].link}{event.material.video[0].description}</div>
-        <div>{event.material.image[0].description}<img src={event.material.image[0].link} /></div> */}
-      {/* <div>organizer {event.organizer}</div>
-      <div>type {event.type}</div>
-      <div>tags {event.tags}</div>
-      <p>feedback</p> */}
-
-
-
-
-
     </Modal>
 
   );
